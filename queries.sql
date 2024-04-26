cd4ee4c8dd570fb11c61a91f4551d1ac0db505e9
Carl <88183738+CarlBerthold@users.noreply.github.com>
2024-04-26 13:52:18 +0200

    select price as vat_price

--- queries.sql
+++ queries.sql
@@ -15,3 +15,14 @@ SELECT DISTINCE category from seminare;
 /* SELECT with a condition and evaluate strict to the where condition: */
 SELECT title, price from seminare where BINARY category = 'framework';
 
+
+/* SELECT with a expression where, or condition and Like */
+SELECT title, description, price from seminare where title like 'RUBY%' or description like '%JAVASCRIPT%';
+
+
+/* SELECT and use maths operations for evaluation first titel and beschreibung than use result for AND  */
+SELECT titel, beschreibung, kategorie FROM seminare WHERE (titel LIKE '%Adobe%' OR beschreibung LIKE '%Adobe%') AND kategorie = 'Webdesign';
+
+
+/* SELECT with a expression and modify the result */
+SELECT title, price, price *1.19 as price_with_vat from seminare;
