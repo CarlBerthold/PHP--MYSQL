fa74181a0c4fa2979f7249b2e8662abb8b059553
Carl <88183738+CarlBerthold@users.noreply.github.com>
2024-05-06 16:03:42 +0200

    numeric and float

--- PHP_TAG_3/grdl18.php
+++ PHP_TAG_3/grdl18.php
@@ -25,4 +25,36 @@ var_dump(is_float(42));
 var_dump(is_float(42.0));
 var_dump(is_float(1e7));
 var_dump(is_float(true));
+
+
+echo floatval(3.14) . PHP_EOL;
+
+echo floatval('abc') . PHP_EOL;
+echo gettype(floatval('abc')) . PHP_EOL;
+
+echo floatval(42) . PHP_EOL;
+echo floatval(42.0) . PHP_EOL;
+echo floatval(1e7) . PHP_EOL;
+echo floatval(true) . PHP_EOL;
+
+$float = '3.14';
+echo gettype(floatval($float)) . PHP_EOL;
+echo gettype(doubleval($float)) . PHP_EOL;
+
+
+
+$var1 = 42;
+$var2 = 3.1415; 
+$var3 = pi();
+$var4 = '42';
+$var4 = ' 42 ';
+$var5 = '3.1415';
+$var6 = 'zweiundvierzig';
+
+var_dump(is_numeric($var1));
+var_dump(is_numeric($var2));
+var_dump(is_numeric($var3));
+var_dump(is_numeric($var4));
+var_dump(is_numeric($var5));
+var_dump(is_numeric($var6));
 ?>
