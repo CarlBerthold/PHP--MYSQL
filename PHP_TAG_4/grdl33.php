91c167a765cf23d15cb39511e5e22a0f584ac0f3
Carl <88183738+CarlBerthold@users.noreply.github.com>
2024-05-07 12:03:57 +0200

    refactor functions

--- PHP_TAG_4/grdl33.php
+++ PHP_TAG_4/grdl33.php
@@ -1,5 +1,5 @@
 <?php
-require 'functions.php';
+require __DIR__ . '/functions.php';
 
 
 // dynamic converting from types with arithmetic operators
--- functions.php
+++ functions.php
@@ -15,18 +15,64 @@ function dump($args)
     echo '</pre>';
 }
 
+$chips = ['5 tüten chips', '3 Flaschen Limonade'];
 
+dump($chips);
+
+// gehoistete Funktion
+greet('Carl');
+
+/**
+ * Undocumented function
+ *
+ * @param [type] $name
+ * @return void
+ */
 function greet($name) 
 {
     echo 'Hello ' . $name;
     return 'Hello ' . $name;
 }
 
+/**
+ * Undocumented function
+ *
+ * @param [type] $name
+ * @param string $salutation
+ * @return void
+ */
 function greet_salutation($name, $salutation = 'Mr.') 
 {
     echo $name . $salutation;
     return $salutation . ' ' . $name;
 }
 
-greet('Carl');
+
 greet_salutation('John');
+
+function test() 
+{
+    echo 'Hello';
+    return 'Hello';
+}
+
+function test2() 
+{
+    echo 'world';
+    return 'Hello';
+}
+
+
+
+// Call a function wihtin another function
+function functionCallInFunction()
+{
+   test();
+   test2();
+}
+
+
+functionCallInFunction();
+
+
+
