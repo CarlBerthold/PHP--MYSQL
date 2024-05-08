b6bda1036db5830039db572a1345218fa286ddbc
Carl <88183738+CarlBerthold@users.noreply.github.com>
2024-05-08 14:44:51 +0200

    logical comparison

--- PHP_BASICS/PHP_TAG_5/comparison/verknuepfung.php
+++ PHP_BASICS/PHP_TAG_5/comparison/verknuepfung.php
@@ -27,3 +27,29 @@ var_dump(TRUE && !TRUE);
 var_dump(!TRUE && FALSE);
 var_dump(TRUE && !FALSE);
 var_dump(TRUE && !TRUE);
+
+
+var_dump((bool) 'Hallo');
+var_dump('Hallo' || FALSE);
+
+
+
+/**
+ * Undocumented function
+ *
+ * @return void
+ */
+function ausgabe() : void
+{
+    var_dump('Hallo Welt!', PHP_EOL);
+}
+
+var_dump(TRUE || ausgabe());
+var_dump(FALSE || ausgabe());
+
+var_dump(TRUE && ausgabe());
+var_dump(FALSE && ausgabe());
+
+
+// logical Terror
+var_dump(((true && false || (int)'Hallo' * 0 && !5 - 14 * (int)'2 Bier!') * 10) !== true);
