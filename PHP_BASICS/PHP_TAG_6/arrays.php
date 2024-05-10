7bd804c9a113f27509ad9c59be3bce749fbace68
Carl <88183738+CarlBerthold@users.noreply.github.com>
2024-05-10 14:20:58 +0200

    array preactise

--- PHP_BASICS/PHP_TAG_6/arrays.php
+++ PHP_BASICS/PHP_TAG_6/arrays.php
@@ -36,9 +36,91 @@ $visitors = [
     ],
 ];
 
-// var_dump($visitors);
-// echo $visitors[0]['name'] . PHP_EOL;
-
-// var_dump($visitors[1]['name'] . ' from ' . $visitors[1]['city']); // string(18) "Jonas from Cologne"
+var_dump($visitors);
+echo $visitors[0]['name'] . PHP_EOL;
+var_dump($visitors[1]['name'] . ' from ' . $visitors[1]['city']); // string(18) "Jonas from Cologne"
 
 echo $visitors[2]['name'] . ' from ' . $visitors[2]['city'] . ' has visited ' . $visitors[2]['visited'][1]; // Vasil from Munich has visited Sidney
+
+// Array Methods
+
+var_dump(in_array('Vasil', $visitors[2], true)); // returns true if the value is in the array
+array_pop($visitors); // removes the last element from the array
+array_shift($visitors); // removes the first element from the array
+array_unshift($visitors); // adds an element to the beginning of the array
+array_push($visitors); // adds an element to the end of the array
+
+
+echo in_array('Vasil', $visitors[2]), PHP_EOL; // returns true if the value is in the array
+echo $visitors[0]['name'];
+
+// array
+$fruits = [
+    'cherry', 
+    'banana', 
+    'apple', 
+    'orange'
+];
+
+// sorting Ascending to the value of the array
+sort($fruits);
+asort($fruits);
+var_dump($fruits);
+
+// associative array
+$associativeFruits = [
+    'c' => 'cherry', 
+    'b' => 'banana', 
+    'a' => 'apple', 
+    'o' => 'orange'
+];
+
+sort($associativeFruits); // sort by value
+asort($associativeFruits); // sort by value
+ksort($associativeFruits); // sort by key
+var_dump($associativeFruits); // array(4) { ["a"]=> string(5) "apple" ["b"]=> string(6) "banana" ["c"]=> string(6) "cherry" ["o"]=> string(6) "orange" }
+
+
+$fruits = [
+    'cherry', 
+    'banana', 
+    'apple', 
+    'orange',
+    'mango',
+    'kiwi',
+    'pear',
+    'peach',
+    'plum'
+];
+
+$moreFruits = [
+    'grape',
+    'strawberry',
+    'raspberry',
+    'blueberry',
+    'blackberry',
+    'currant',
+    'gooseberry',
+    'pomegranate',
+    'pineapple',
+    'papaya',
+    'passion fruit'
+];
+
+
+$fruits = array_merge($fruits, $moreFruits); // merge two arrays concat them
+var_dump($fruits);
+
+$fruits = [...$fruits, ...$moreFruits]; // merge two arrays concat them
+echo $str = implode(", ", $fruits);
+var_dump($fruits);
+
+
+
+$numbers = [1, 2, 3, 4, 4, 7, 8, 9];
+// re declare the value of the 4 index to 5
+$numbers[4] = 5;
+// take array, index, length, replace value
+array_splice($numbers, 5, 0, [6]);
+
+var_dump($numbers);
