60eff4e06d1d2e99737cbd240da55c084dab27a1
Carl <88183738+CarlBerthold@users.noreply.github.com>
2024-05-10 16:28:30 +0200

    refactor options for the result

--- PHP_BASICS/AUFGABEN/comparison/AUFGABE_2.php
+++ PHP_BASICS/AUFGABEN/comparison/AUFGABE_2.php
@@ -1,22 +1,41 @@
 <?php
 
-$firstname = "Max";
-$lastname = "mustermann";
+$firstName = "Max";
+$lastName = "mustermann";
 $street = "Musterstraße";
 $houseNumber = 42;
 $city = "Musterstadt";
 
+// echo $jsonString = $firstName . PHP_EOL . $lastName . PHP_EOL . $street . ' ' . $houseNumber . PHP_EOL . $city . PHP_EOL;
+
+
+$firstKey = "firstname";
+$secondKey = "lastname";
+$thirdKey = "street";
+$fourthKey = "houseNumber";
+$fifthKey = "city";
+
+// echo $jsonToString = " \"{$firstKey}\" : \"{$firstName}\" " . PHP_EOL . " \"{$secondKey}\" : \"{$lastName}\"" . PHP_EOL . "\"{$thirdKey}\" : \"{$street}\"" . PHP_EOL . "\"{$fourthKey}\" : \"{$houseNumber}\"" . PHP_EOL . "\"{$fifthKey}\" : \"{$city}\"" . PHP_EOL;
+
+
+$jsonString = 
+"{
+    \"$firstKey\": \"$firstName\",
+    \"$secondKey\": \"$lastName\",
+    \"$thirdKey\": \"$street\",
+    \"$fourthKey\": $houseNumber,
+    \"$fifthKey\": \"$city\"
+}";
+
+ echo ($jsonString);
+
 
 $address = [
-    "firstname" => $firstname,
-    "lastname" => $lastname,
+    "firstname" => $firstName,
+    "lastname" => $lastName,
     "street" => $street,
     "houseNumber" => $houseNumber,
     "city" => $city
 ];
-
-
-
-echo $jsonString = $firstname . PHP_EOL . $lastname . PHP_EOL . $street . PHP_EOL . $houseNumber . PHP_EOL . $city . PHP_EOL;
 
 echo json_encode($address, JSON_PRETTY_PRINT);
