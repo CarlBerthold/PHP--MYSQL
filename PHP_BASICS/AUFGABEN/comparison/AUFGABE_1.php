437cf636f8186ee05ce2913a3e0796a9b7c59dd2
Carl <88183738+CarlBerthold@users.noreply.github.com>
2024-05-10 16:01:54 +0200

    refactor

--- PHP_BASICS/AUFGABEN/comparison/AUFGABE_1.php
+++ PHP_BASICS/AUFGABEN/comparison/AUFGABE_1.php
@@ -1,5 +1,10 @@
 <?php
 $customerName = 'John Doe';
-$orderValue = 150;
-$discount = $orderValue - 100;
-echo "Customer: $customerName" . PHP_EOL . "Order Value: $orderValue" . PHP_EOL . "Discount: $discount" . PHP_EOL;
+$orderValue = 237.30;
+$discount = 100.00;
+$result = $orderValue - $discount;
+
+
+echo "Customer: $customerName" . PHP_EOL . "Order Value: $orderValue" . PHP_EOL . "Discount: $discount" . PHP_EOL . "Total: " . $result . PHP_EOL;
+
+// echo "Customer: $customerName" . PHP_EOL . "Order Value: $orderValue" . '$'. PHP_EOL . "Discount: $discount" . '$' . PHP_EOL . "Total: " . ($orderValue - $discount) . '$' . PHP_EOL;
