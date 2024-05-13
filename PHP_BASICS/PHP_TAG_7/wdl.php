63aee073f9081c0fc3de7bc57804c19d3c0ec8d2
Carl <88183738+CarlBerthold@users.noreply.github.com>
2024-05-13 13:06:42 +0200

    add docblocks

--- PHP_BASICS/PHP_TAG_7/wdl.php
+++ PHP_BASICS/PHP_TAG_7/wdl.php
@@ -1,6 +1,6 @@
 <?php
 
-/* $number = 4;
+$number = 4;
 
 if ($number > 4) {
     echo 'The number is bigger than 4', PHP_EOL;
@@ -8,10 +8,10 @@ if ($number > 4) {
     echo 'The number is smaller than 4', PHP_EOL;
 } else {
     echo 'The number is equal to 4', PHP_EOL;
-} */
+}
 
 
-/* if ($number) {
+if ($number) {
     switch ($number) {
         case 1:
             echo 'The number is 1', PHP_EOL;
@@ -28,19 +28,33 @@ if ($number > 4) {
         default:
             echo 'The number is not 1, 2, 3 or 4', PHP_EOL;
     }
-} */
+}
 
 
  $name = 'John';
 //$name = '';
 //$name = NULL;
 
+
+
+/**
+ * Checks if the variable $name is not empty and prints the corresponding message.
+ *
+ * @param string $name The name to be checked.
+ * @return void
+ */
 if ($name) {
     echo 'The name is ' . $name, PHP_EOL;
 } else {
     echo 'The name is empty', PHP_EOL;
 }
 
+/**
+ * Checks if the variable $name is not empty and displays a message accordingly.
+ *
+ * @param string $name The name to be checked.
+ * @return void
+ */
 if (!empty($name)) {
     echo 'The name is ' . $name, PHP_EOL;
 } else {
@@ -49,4 +63,11 @@ if (!empty($name)) {
 
 // $name ? true : false;
 
+/**
+ * This code snippet prints the value of the variable $name if it is not empty.
+ * If $name is empty, it prints a message indicating that the name is empty.
+ *
+ * @param string $name The name to be printed.
+ * @return void
+ */
 echo $name ? 'The name is ' . $name : 'The name is empty', PHP_EOL;
