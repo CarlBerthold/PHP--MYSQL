08940d53f158aaa7b99093ad64d61ef4641345cf
Carl <88183738+CarlBerthold@users.noreply.github.com>
2024-05-16 16:48:03 +0200

    refactor calculator to partial files

---
+++ PHP_BASICS/PHP_TAG_11/head.php
@@ -0,0 +1,6 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <title>Calculator</title>
+</head>
