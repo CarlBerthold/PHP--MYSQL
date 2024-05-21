82f1b2aaf9e53b2a8e147f173dd96f3c878482e9
Carl <88183738+CarlBerthold@users.noreply.github.com>
2024-05-21 14:19:50 +0200

    requests and logins

---
+++ PHP_BASICS/PHP_TAG_12/fail.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>fail</title>
+</head>
+<body>
+
+    <h1>Fail</h1>
+    <p>Username or password is incorrect</p>
+    <a href="login.php">Back to login</a> 
+    
+</body>
+</html>
--- PHP_BASICS/PHP_TAG_12/login.php
+++ PHP_BASICS/PHP_TAG_12/login.php
@@ -1,6 +1,51 @@
 <?php
 
-var_dump($_POST);
+/* var_dump($_POST);
+var_dump($_GET);
+
+var_dump($_SERVER['REQUEST_METHOD'] === 'POST'); */
+
+$userData = [
+    [
+        'username' => 'admin',
+        'password' => 'admin',
+    ],
+    [
+        'username' => 'user',
+        'password' => 'user',
+    ],
+    [
+        'username' => 'test',
+        'password' => 'test',
+    ],
+];
+
+
+// using foreach loop
+if($_POST) {
+    foreach($userData as $user) {
+        if($user['username'] === $_POST['username'] && $user['password'] === $_POST['password']) {
+            header('Location: success.php');
+            echo "Login successful" . ' ' .  $user['username'];
+            exit;
+        }
+    }
+}
+
+// using array methods
+$loginNames = array_column($userData, 'loginname');
+
+if ($_POST) {
+    if (($index = array_search($_POST['loginname'], $loginNames)) !== false) {
+        if ($_POST['password'] === $userData[$index]['password']) {
+            // hier Weiterleitung auf die gewünschte Seite
+            echo "Hallo {$user['loginname']} ! Du bist eingelogged";
+            exit;
+        }
+    }
+
+    $message = 'Die Kombination Benutzername und Kennwort stimmen nicht überein!';
+}
 
 
 ?>
@@ -18,11 +63,19 @@ var_dump($_POST);
     <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
         <div>
             <label for="username">Username:</label>
-            <input type="text" name="username" id="username" value="">
+            <input 
+                type="text" 
+                name="username" 
+                id="username" 
+                value="<?= isset($_POST['username'])? $_POST['username'] : '' ?>">
         </div>
         <div>
             <label for="password">Password:</label>
-            <input type="password" name="password" id="password" value="">
+            <input 
+                type="password" 
+                name="password" 
+                id="password" 
+                value="<?php $_POST ?? ''?>">
         </div>
         <button type="submit">Login</button>
     </form>
