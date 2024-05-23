b936214f03194a9feba1c9fa8c83c9c3288482e3
Carl <88183738+CarlBerthold@users.noreply.github.com>
2024-05-23 14:55:58 +0200

    refactor faculty function

--- PHP_BASICS/PHP_TAG_14/Fakultät.php
+++ PHP_BASICS/PHP_TAG_14/Fakultät.php
@@ -13,6 +13,13 @@
 n! = 1 * 2 * 3 * ... * n */
 
 
+/**
+ * Calculates the factorial of a given number using iteration.
+ * This function uses a for loop.
+ *
+ * @param int $n The number for which to calculate the factorial.
+ * @return int The factorial of the given number.
+ */
 function faculty($n) {
     $result = 1;
     for($i = 1; $i <= $n; $i++) {
@@ -24,7 +31,14 @@ function faculty($n) {
 echo faculty(6);
 
 
-function recursiveFaculty ($n) {
+/**
+ * Calculates the factorial of a given number using recursion.
+ * This function uses an if-else statement.
+ *
+ * @param int $n The number for which the factorial needs to be calculated.
+ * @return int The factorial of the given number.
+ */
+function recursiveFaculty (int $n = 1) : int {
     if($n > 1) {
         $result = recursiveFaculty($n - 1) * $n;
     } else {
@@ -32,7 +46,73 @@ function recursiveFaculty ($n) {
     }
     return $result;
 }
-
 echo recursiveFaculty(3);
 
 
+/**
+ * Calculates the factorial of a given number using recursion.
+ * This function uses the ternary operator.
+ *
+ * @param int $n The number for which the factorial needs to be calculated.
+ * @return int|float The factorial of the given number.
+ */
+function recursiveFaculty2 (int $n = 1) : int|float {
+    return ($n > 1) ? recursiveFaculty2($n - 1) * $n : $n;
+}
+
+echo recursiveFaculty2(6);
+
+// anonymous function with variable
+// parent scope varibale via use keyword
+// 
+/**
+ * Calculates the factorial of a given number recursively.
+ * This function uses an anonymous function.
+ *
+ * @param int $n The number to calculate the factorial for.
+ * @return int The factorial of the given number.
+ */
+$recursiveFaculty2 = function (int $n) use (&$recursiveFaculty2) {
+    return ($n > 1) ? $recursiveFaculty2($n - 1) * $n : $n;
+};
+echo $recursiveFaculty2(6);
+
+
+// arrow function
+// parent varibale need to be passed as argument
+// recursive call function need also to be passed the parent varibale as argument
+/**
+ * Calculates the factorial of a given number using recursion.
+ *
+ * @param callable $recursiveFaculty3 The recursive function to calculate the factorial.
+ * @param int $n The number to calculate the factorial for.
+ * @return int The factorial of the given number.
+ */
+$recursiveFaculty3 = fn ($recursiveFaculty3, int $n) => ($n > 1) ? $recursiveFaculty3($recursiveFaculty3, $n - 1) * $n : $n;
+
+echo $recursiveFaculty3($recursiveFaculty3, 6);
+
+
+
+/**
+ * currying
+ * Calculates the factorial of a given number.
+ * we dont need to parse the the varibale to the function
+ * 
+ * @param int $fac The factorial function.
+ * @return int The factorial of the given number.
+ */
+$fac = fn($fac) => fn($n = 1) => ($n > 1) ? $fac($fac)($n - 1) * $n : 1;
+
+/**
+ * currying
+ * Calculates the factorial of a given number.
+ * we call the function with the number only
+ *
+ * @param int $n The number to calculate the factorial for.
+ * @return int The factorial of the given number.
+ */
+$faculty = $fac($fac);
+
+echo $faculty(6) . PHP_EOL;
+echo $faculty(20) . PHP_EOL;
