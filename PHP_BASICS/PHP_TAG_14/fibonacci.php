ddc3af751b329eaa1a481138ad3061ed455f4a42
Carl <88183738+CarlBerthold@users.noreply.github.com>
2024-05-24 08:50:54 +0200

    add some comments to the fibonacci functions and try to make perfomrance test

--- PHP_BASICS/PHP_TAG_14/fibonacci.php
+++ PHP_BASICS/PHP_TAG_14/fibonacci.php
@@ -9,7 +9,7 @@ function fibonacci($n = 2) {
     }
  } 
    
- echo fibonacci(6);
+ echo fibonacci(20); //6765 [Done] exited with code=0 in 0.635 seconds
 
 
 
@@ -22,7 +22,7 @@ function fibonacci2($n = 2) {
     return ($n < 2) ? $n : fibonacci2($n - 1) + fibonacci2($n - 2);
 }
 
-echo fibonacci2(6);
+echo fibonacci2(20); //6765 [Done] exited with code=0 in 0.159 seconds
 
 
 /**
@@ -35,13 +35,18 @@ echo fibonacci2(6);
  */
 function optimizedFibonacci(int $n = 2) {
     if($n > 2) {
-        $fibonacci = [0, 1];
+        // initialize the first two numbers of the Fibonacci sequence bcs we know them and they dont have to be calculated again and again
+        $fibonacci = [0, 1]; 
+        // iterate through the numbers except the first and second number of the Fibonacci
         for ($i = 2; $i <= $n; $i++) {
+            // assigning the first and second number of the Fibonacci to the next number of the iteration 
             $fibonacci[$i] = $fibonacci[$i - 1] + $fibonacci[$i - 2];
         }
+        // return the Fibonacci number at the given position 
         return $fibonacci[$n];
     }
+    // return the Fibonacci number at the given position
     return $n;
 }
 
-echo optimizedFibonacci(6);
+echo optimizedFibonacci(20); // 6765 [Done] exited with code=0 in 0.135 seconds
