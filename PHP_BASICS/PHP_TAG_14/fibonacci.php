602a9f69854441cdb99b63248f9be4db2b2e3066
Carl <88183738+CarlBerthold@users.noreply.github.com>
2024-05-27 16:22:12 +0200

    update fibonacci

--- PHP_BASICS/PHP_TAG_14/fibonacci.php
+++ PHP_BASICS/PHP_TAG_14/fibonacci.php
@@ -2,12 +2,12 @@
 
 // Fibonacci sequence via recursion
 function fibonacci($n = 2) {
-    if ($n < 2) {
+    if ($n <= 2) {
         return $n;
     } else {
         return fibonacci($n - 1) + fibonacci($n - 2);
     }
- } 
+}
    
  echo fibonacci(20); //6765 [Done] exited with code=0 in 0.635 seconds
 
@@ -19,7 +19,7 @@ function fibonacci($n = 2) {
 // https://www.youtube.com/watch?v=e0CAbRVYAWg
  
 function fibonacci2($n = 2) {
-    return ($n < 2) ? $n : fibonacci2($n - 1) + fibonacci2($n - 2);
+    return ($n <= 2) ? $n : fibonacci2($n - 1) + fibonacci2($n - 2);
 }
 
 echo fibonacci2(20); //6765 [Done] exited with code=0 in 0.159 seconds
@@ -36,17 +36,60 @@ echo fibonacci2(20); //6765 [Done] exited with code=0 in 0.159 seconds
 function optimizedFibonacci(int $n = 2) {
     if($n > 2) {
         // initialize the first two numbers of the Fibonacci sequence bcs we know them and they dont have to be calculated again and again
-        $fibonacci = [0, 1]; 
+        $fibonacciStart = [0, 1]; 
         // iterate through the numbers except the first and second number of the Fibonacci
         for ($i = 2; $i <= $n; $i++) {
             // assigning the first and second number of the Fibonacci to the next number of the iteration 
-            $fibonacci[$i] = $fibonacci[$i - 1] + $fibonacci[$i - 2];
+            $fibonacciStart[$i] = $fibonacciStart[$i - 1] + $fibonacciStart[$i - 2];
         }
         // return the Fibonacci number at the given position 
-        return $fibonacci[$n];
+        return $fibonacciStart[$n];
     }
     // return the Fibonacci number at the given position
     return $n;
 }
 
 echo optimizedFibonacci(20); // 6765 [Done] exited with code=0 in 0.135 seconds
+
+function optimizedFibonacci2(int $n = 2) {
+    if($n > 2) {
+        // initialize the first three numbers of the Fibonacci sequence
+        $fibonacciStart = [0, 1, 1]; 
+        // iterate through the numbers except the first three numbers of the Fibonacci
+        for ($i = 3; $i <= $n; $i++) {
+            // assigning the sum of the two preceding numbers of the Fibonacci to the next number of the iteration 
+            $fibonacciStart[$i] = $fibonacciStart[$i - 1] + $fibonacciStart[$i - 2];
+        }
+        // return the Fibonacci number at the given position 
+        return $fibonacciStart[$n];
+    }
+    // return the Fibonacci number at the given position
+    return $n <= 1 ? $n : 1;
+}
+
+echo optimizedFibonacci2(20); // 6765 [Done] exited with code=0 in 0.135 seconds
+
+
+
+
+// Fibonacci sequence using variabels and assigning the pattern 1, 2 and 3
+//  "O(n) steps and O(1) space" // can scale 1:1 with the input
+function optimizedFibonacci3(int $n = 2) {
+    if ($n <= 1) {
+        return $n;
+    }
+
+    $zero = 0; // F(0)
+    $first = 1;    // F(1)
+    $result = 1;  // F(2)
+
+    for ($i = 3; $i <= $n; $i++) {
+        $zero = $first;
+        $first = $result;
+        $result = $zero + $first;
+    }
+
+    return $result;
+}
+
+echo optimizedFibonacci3(20); // 6765
