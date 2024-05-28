dd4a76524bb91de2488349fcfa5e1bdb7123a002
Carl <88183738+CarlBerthold@users.noreply.github.com>
2024-05-28 09:56:08 +0200

    refactor fibonacci and towers of hanoi

--- PHP_BASICS/PHP_TAG_14/fibonacci.php
+++ PHP_BASICS/PHP_TAG_14/fibonacci.php
@@ -1,6 +1,36 @@
 <?php
 
+/**
+ * Calculates the Fibonacci sequence up to a given number.
+ * This function uses recursion to calculate the Fibonacci sequence but its not efficient
+ * the if condition could be optimized $n <= 2
+ * 
+ *
+ * @param int $n The number up to which the Fibonacci sequence should be calculated.
+ * @return int The Fibonacci number at position $n.
+ */
+function fibonacci0($n)
+{
+    if (($n == 1) || ($n == 0)) {
+        return $n;
+    }
+    return fibonacci0($n - 1) + fibonacci0($n - 2);
+}
+
+$number = 10;
+for ($counter = 0; $counter < $number; $counter++) {
+    echo fibonacci0($counter), ' ';
+}
+
+echo fibonacci0(35); // 6765 [Done] exited with code=0 in 0.635 seconds
+
 // Fibonacci sequence via recursion
+/**
+ * Calculates the Fibonacci sequence up to a given number.
+ *
+ * @param int $n The number up to which the Fibonacci sequence should be calculated.
+ * @return int The Fibonacci sequence up to the given number.
+ */
 function fibonacci($n = 2) {
     if ($n <= 2) {
         return $n;
@@ -28,7 +58,6 @@ echo fibonacci2(20); //6765 [Done] exited with code=0 in 0.159 seconds
 /**
  * Calculates the Fibonacci number at the given position using an optimized approach.
  * This function uses a for loop to iterate through the numbers execept the first and second number of the Fibonacci.
- * if we use recursion we have to calculate the same number multiple times
  *
  * @param int $n The position of the Fibonacci number to calculate (default is 2).
  * @return int The Fibonacci number at the given position.
@@ -51,6 +80,13 @@ function optimizedFibonacci(int $n = 2) {
 
 echo optimizedFibonacci(20); // 6765 [Done] exited with code=0 in 0.135 seconds
 
+/**
+ * Calculates the Fibonacci number at the given position using an optimized approach.
+ * This function uses a for loop to iterate through the numbers execept the first three numbers of the Fibonacci.
+ *
+ * @param int $n The position of the Fibonacci number to calculate (default is 2).
+ * @return int The Fibonacci number at the given position.
+ */
 function optimizedFibonacci2(int $n = 2) {
     if($n > 2) {
         // initialize the first three numbers of the Fibonacci sequence
@@ -74,6 +110,15 @@ echo optimizedFibonacci2(20); // 6765 [Done] exited with code=0 in 0.135 seconds
 
 // Fibonacci sequence using variabels and assigning the pattern 1, 2 and 3
 //  "O(n) steps and O(1) space" // can scale 1:1 with the input
+/**
+ * Calculates the Fibonacci number at the given position using an optimized iterative approach.
+ * This function uses a for loop to iterate through the numbers except the first three numbers of the Fibonacci.
+ * we are using varibales instead of a complex data structure
+ * we shift the values of the variables to the right and iterate just once through the numbers
+ *
+ * @param int $n The position of the Fibonacci number to calculate (default: 2).
+ * @return int The Fibonacci number at the given position.
+ */
 function optimizedFibonacci3(int $n = 2) {
     if ($n <= 1) {
         return $n;
@@ -92,4 +137,16 @@ function optimizedFibonacci3(int $n = 2) {
     return $result;
 }
 
-echo optimizedFibonacci3(20); // 6765
+echo optimizedFibonacci3(50); // 6765
+
+
+function optioptiFibonacci() 
+{
+    static $value = 0;
+
+    echo $value, PHP_EOL;
+
+    $value++;
+
+    return $value;
+}
