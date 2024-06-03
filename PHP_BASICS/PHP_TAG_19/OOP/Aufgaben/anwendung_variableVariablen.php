8a51a956dfae0110cd4b8f9b8024538a9f65b03c
Carl <88183738+CarlBerthold@users.noreply.github.com>
2024-06-03 20:33:31 +0200

    Create anwendung_variableVariablen.php

---
+++ PHP_BASICS/PHP_TAG_19/OOP/Aufgaben/anwendung_variableVariablen.php
@@ -0,0 +1,36 @@
+<?php
+
+$article = [
+    [
+        'language' => 'de',
+        'name'=>'Auto',
+        'color'=>'rot',
+        'price' => 25000,
+        'currency' => 'Euro'
+    ],
+    [
+        'language' => 'en',
+        'name'=>'car',
+        'color'=>'red',
+        'price' => 26500,
+        'currency' => 'Dollar'
+    ],
+    [
+        'language' => 'fr',
+        'name'=>'voiture',
+        'color'=>'rouge',
+        'price' => 25000,
+        'currency' => 'Euro'
+    ],
+    [
+        'language' => 'it',
+        'name'=>'automobile',
+        'color'=>'rosso',
+        'price' => 25000,
+        'currency' => 'Euro'
+    ],
+]; 
+
+# auf der Webseite soll je nach Spracheinstellung z. B. folgende Ausgabe erfolgen
+
+// Ausgabe: Auto, rot: 25000 Euro
