0413f9b55ac3728ea9b8a25db5870999ddca10df
Carl <88183738+CarlBerthold@users.noreply.github.com>
2024-06-04 14:45:15 +0200

    getter / setter

---
+++ PHP_BASICS/OOP/Aufgaben/Book.php
@@ -0,0 +1,99 @@
+<?php
+
+class Book 
+{
+    private $title = '';
+    
+    private $description = '';
+
+    private $auhtor = '';
+
+    private $genre = '';
+
+    protected $price = Null;
+    
+    
+    
+    //GETTER
+    public function getTitle() 
+    {
+        return $this->title ;
+    }
+    public function getDescription() 
+    {
+        return $this->description;
+    }
+    public function getAuthor() 
+    {
+        return $this->auhtor ;
+    }
+    public function getGenre() 
+    {
+        return $this->genre ;
+    }
+    public function getPrice($netto = false, $brutto = true) 
+    {
+        if($brutto) {
+            return $this->price;
+        }
+
+        if($netto) {
+            $price = $this->price;
+            return $this->getNettoPrice($price);
+        }
+       
+    }
+    //SETTER
+    public function setTitle(string $value) :  object 
+    {
+        $this->title = $value;
+        return $this;
+    }
+    public function setDescription(string $value) : object 
+    {
+        $this->description = $value;
+        return $this;
+    }
+
+    public function setAuthor(string $value) :object 
+    {
+        $this->auhtor = $value;
+        return $this;
+    }
+    public function setGenre(string $value) :  object 
+    {
+        $this->genre = $value;
+        return $this;
+    }
+    public function setPrice(int|float $value) : int|float|object
+    {
+        $this->price = $value * 1.19;
+        return $this;
+    }
+
+
+    // METHODS
+    protected function getNettoPrice($price) 
+    {
+        $this.$price = $this->price - ($this->price * 0.19);
+        return $this.$price;
+    }
+}
+
+// Create a new Book 
+$theManifest = new Book();
+$title = $theManifest->setTitle('Per Anhalter durch die Galaxy');
+$description = $theManifest->setDescription('Per Anhalter durch die Galaxis, Originaltitel The Hitchhiker’s Guide to the Galaxy,...');
+$author = $theManifest->setAuthor('Douglas Adams');
+$genre = $theManifest->setGenre('comedy');
+$price = $theManifest->setPrice(5.00);
+
+var_Dump($theManifest);
+
+$title = $theManifest->getTitle();
+$description = $theManifest->getDescription();
+$author = $theManifest->getAuthor();
+$genre = $theManifest->getGenre();
+$price = $theManifest->getPrice();
+
+var_dump($title, $description, $author, $genre, $price);
---
+++ PHP_BASICS/PHP_TAG-20/Pasta.php
@@ -0,0 +1,10 @@
+<?php
+
+
+class Pasta 
+{
+    public $name = '';
+    public $nutri = '';
+    public $price; // $price = NULL;
+    public $amount = 0;
+}
